4e1941a6fd2ab754def509598ec1f3363862441d
yhylord <yhylord@gmail.com>
2019-01-13 23:59:42 +0800

    Follow links and see if dead

--- traphorns/spiders/findtraps.py
+++ traphorns/spiders/findtraps.py
@@ -5,6 +5,14 @@ import scrapy
 class FindtrapsSpider(scrapy.Spider):
     name = 'findtraps'
     allowed_domains = ['utexas.edu']
-    start_urls = ['http://utexas.edu/']
+    start_urls = ['http://utexas.edu']
 
     def parse(self, response):
+        if response.status == 404:
+            yield {
+                'dead_link': response.request.meta.get('redirect_urls',
+                                                       [response.url])[0]
+            }
+        else:
+            for link in response.css('a::attr(href)'):
+                yield response.follow(link, callback=self.parse)
