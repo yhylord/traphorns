349c4cdbc36fdee360a1a42e2565d35445bf6a28
yhylord <yhylord@gmail.com>
2019-01-18 00:09:29 +0800

    Integrate with SQLite

--- traphorns/pipelines.py
+++ traphorns/pipelines.py
@@ -4,8 +4,33 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
+from scrapy.exceptions import DropItem
 
 
 class TraphornsPipeline(object):
+
+    table_name = 'links'
+
+    def __init__(self, db_filename):
+        self.db_filename = db_filename
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.get('DB_FILENAME', 'traps.db'))
+
+    def open_spider(self, spider):
+        self.conn = sqlite3.coneect(self.db_filename)
+        self.db = self.conn.cursor()
+
+    def close_spider(self, spider):
+        self.conn.close()
+
     def process_item(self, item, spider):
-        return item
+        if item['source']:
+            columns = (self.table_name,
+                       item['source'], item['link'], item['dead'])
+            self.db.execute('INSERT INTO ? VALUES (?,?,?)', columns)
+            return item
+        else:  # no source, must be one of start urls
+            raise DropItem('Omit start url, item: {}'.format(item))
